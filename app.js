0e4d37c9bb813d3888157d3336e5b0c9216f58a0
Amirhosein Moqaddasi <ah.moqaddasi@gmail.com>
2025-01-10 16:05:02 +0330

    Add CSS styling and restructure HTML for improved layout and navigation

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const adminRoutes = require('./routes/admin')
 const shopRoutes = require('./routes/shop')
 const app = express();
 
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(bodyParser.urlencoded({ extended: false }))
 
